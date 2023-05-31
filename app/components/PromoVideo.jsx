99834f1a4ef37ea0c747b6bc22b1959b3276a989
nomandhoni-cs <nomandhoni@yahoo.com>
2023-05-31 19:05:06 +0600

    Finished iframe section

--- app/components/PromoVideo.jsx
+++ app/components/PromoVideo.jsx
@@ -4,18 +4,17 @@ const PromoVideo = () => {
   const videoId = "jDIs7GivgGc";
 
   return (
-    <div className="sm:flex sm:flex-row items-center">
-      <div className="iframe-promo h-96 w-full sm:w-1/2 sm:px-10">
+    <div className="lg:flex lg:flex-row items-center lg:mb-14">
+      <div className="iframe-promo lg:h-96 w-full lg:w-full lg:px-10 px-0 mb-3 lg:mb-0">
         <iframe
-          className="w-full h-72 rounded-lg shadow-lg"
+          className="w-full lg:h-full sm:h-96 h-56 rounded-lg shadow-lg"
           src={`https://www.youtube.com/embed/${videoId}`}
           title="Promo Video"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
       </div>
-      <div className="iframe-info h-96 sm:w-1/2">
-        <h2 className="text-4xl font-semibold text-gray-900 mb-4">
+      <div className="iframe-info h-80 lg:w-3/5">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">
           Check out our Promo Video
         </h2>
         <p className="text-gray-700">
@@ -23,8 +22,7 @@ const PromoVideo = () => {
           video. Immerse yourself in a visual journey that showcases the key
           features, benefits, and unique aspects of our offering. Get a sneak
           peek into the exciting world of our product and discover how it can
-          transform your experience. Join us on this engaging adventure and
-          unleash the possibilities with our remarkable solution.
+          transform your experience.
         </p>
       </div>
     </div>
