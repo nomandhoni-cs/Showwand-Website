bc79aef22e34a527dec1aa4ac81730075cd76122
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2023-07-14 15:39:48 +0000

    Fixed deployment failed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import OpenSource from "@/components/OpenSource";
-import CodeLanguageStats from "@/components/CodeLanguageStats";
-import PromoVideo from "@/components/PromoVideo";
+// import OpenSource from "@/components/OpenSource";
+// import CodeLanguageStats from "@/components/CodeLanguageStats";
+// import PromoVideo from "@/components/PromoVideo";
 import HeroSection from "@/components/HeroSection";
-import Contributors from "@/components/Contributors";
-import KeyFeatures from "@/components/KeyFeatures";
+// import Contributors from "@/components/Contributors";
+// import KeyFeatures from "@/components/KeyFeatures";
 
 const Home:React.FC = () => {
   return (
